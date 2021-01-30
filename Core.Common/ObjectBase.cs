90f555c404834cb4cd40ca2fa0093fcf1b34cdca
Ahmed Bizo <ahmedmajedb@hotmail.com>
2021-01-30 01:44:17 +0100

    core comman

---
+++ Core.Common/ObjectBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace Core.Common
+{
+    public abstract class ObjectBase
+    {
+        public static CompositionContainer Container { get; set; }
+    }
+}
