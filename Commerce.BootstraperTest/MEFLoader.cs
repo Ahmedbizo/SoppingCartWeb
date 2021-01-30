0685c97f76f3878f889b3681ab75b88095c91991
Ahmed Bizo <ahmedmajedb@hotmail.com>
2021-01-30 01:41:41 +0100

    Test

---
+++ Commerce.BootstraperTest/MEFLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCart.Repositories.Repositories;
+
+namespace Commerce.BootstraperTest
+{
+    public static class MEFLoader
+    {
+        public static CompositionContainer Init()
+        {
+            AggregateCatalog catalog = new AggregateCatalog();
+
+            catalog = addCatalogItems(catalog);
+            CompositionContainer container = new CompositionContainer(catalog);
+
+            return container;
+        }
+
+
+        public static AggregateCatalog addCatalogItems(AggregateCatalog catalog)
+        {
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(DepartmentRepository).Assembly));
+            return catalog;
+        }
+
+    }
+}
